b0ef9f549ce40ed17ad6debaeee4a44b4cfe70ef
Scr18 <nguyenquanghuyx8@gmail.com>
2023-05-21 22:57:09 +0700

    fix: router

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 const app = express();
-
+app.use(cors());
 export const viteNodeApp = app;
--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/client/home/home.component';
+import { LayOutClientComponent } from './layouts/client/lay-out-client/lay-out-client.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayOutClientComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
