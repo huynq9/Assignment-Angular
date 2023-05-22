546cf315cf576e8b1342462380900b56ef2da064
Scr18 <nguyenquanghuyx8@gmail.com>
2023-05-23 05:07:29 +0700

    feat: [BE] CRUD, reactor: [UI]

---
+++ backend/src/controllers/products.js
@@ -0,0 +1,46 @@
+import Product from "../models/products";
+import Categories from "../models/categories";
+
+export const get = async (req, res) => {
+  try {
+    if (req.params.id) {
+      const product = await Product.findById(req.params.id).populate(
+        "categoryId"
+      );
+      if (!product) {
+        return res.status(404).json({ message: "product not found" });
+      }
+      return res.status(200).json({ message: "product found", product });
+    }
+    const products = await Product.find({});
+    if (products.length === 0) {
+      return res.status(404).json({ message: "products not found" });
+    }
+    return res.status(200).json({ message: "products found", products });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const product = Product.create(req.body);
+    if (!product) {
+      return res.status(404).json({ message: "can't create product" });
+    }
+    await Categories.findByIdAndUpdate(product.categoryId, {
+      $addToSet: {
+        products: product._id,
+      },
+    });   
+    return res.status(200).json({
+      message: "create sucessfull",
+      product,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error });
+  }
+};
+export const update = async (req, res) => {
+  
+}
