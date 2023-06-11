cb46902d7d1e997ad2b47703f0e9e86f54c32c59
your name <abc@gmail.com>
2023-06-11 10:04:54 +0700

    .

--- my-app/src/app/services/auth.service.ts
+++ my-app/src/app/services/auth.service.ts
@@ -10,7 +10,7 @@ export class AuthService {
   apiurl='http://localhost:9999/user';
 
   constructor(private http:HttpClient) { }
-  
+
   RegisterUser(inputdata:any){
     return this.http.post(this.apiurl,inputdata)
   }
@@ -24,7 +24,7 @@ export class AuthService {
     return this.http.put(this.apiurl+'/'+id,inputdata);
   }
   getuserrole(){
-    return this.http.get('http://localhost:3000/role');
+    return this.http.get('http://localhost:9999/role');
   }
   isloggedin(){
     return sessionStorage.getItem('username')!=null;
@@ -33,9 +33,9 @@ export class AuthService {
     return sessionStorage.getItem('role')!=null?sessionStorage.getItem('role')?.toString():'';
   }
   GetAllCustomer(){
-    return this.http.get('http://localhost:3000/customer');
+    return this.http.get('http://localhost:9999/customer');
   }
   Getaccessbyrole(role:any,menu:any){
-    return this.http.get('http://localhost:3000/roleaccess?role='+role+'&menu='+menu)
+    return this.http.get('http://localhost:9999/roleaccess?role='+role+'&menu='+menu)
   }
 }
